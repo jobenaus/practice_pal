cfdd53deeca876eb627b62e675ee112198120d47
jobenaus <j.obenaus@icloud.com>
2023-04-21 21:50:18 +0200

    feat(anki): implement basic error handling in invokeWithErrors function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import Head from "next/head";
 import Link from "next/link";
 
-import { useConnectToAnki } from "./api/anki_connect/anki_connect";
+import { connectToAnki } from "./api/anki_connect/anki_connect";
 
 import { api } from "@/utils/api";
 import { type NextPage } from "next";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
-  useConnectToAnki();
 
   return (
     <>
@@ -23,6 +21,9 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
+          <div>
+            <button onClick={connectToAnki}>Click me</button>
+          </div>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
