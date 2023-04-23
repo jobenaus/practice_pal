1d2477e70073ba90b5d03cf421daec89b336bf93
jobenaus <j.obenaus@icloud.com>
2023-04-23 15:41:41 +0200

    feat(index): add form component to page using React Hot Toast for notifications

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import Head from "next/head";
-
-import { connectToAnki } from "./api/anki_connect/anki_connect";
+import toast, { Toaster } from "react-hot-toast";
 
 import { type NextPage } from "next";
-import { useState } from "react";
 
 const Home: NextPage = () => {
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+  const notify = () => toast("Here is your toast.");
   return (
     <>
       <Head>
@@ -20,48 +18,50 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Practice Pal
           </h1>
-          <div>
-            <button
-              className="rounded-lg bg-indigo-300 p-5"
-              onClick={connectToAnki}
-            >
-              Connect to Anki
-            </button>
-          </div>
-          <div>
-            <label
-              htmlFor="deck-title"
-              className="block text-sm font-medium leading-6 text-white"
-            >
-              Deck Title
-            </label>
-            <div className="relative mt-2 rounded-md shadow-sm">
-              <input
-                type="text"
-                name="deck-title"
-                id="deck-title"
-                className="block w-96 rounded-md border-0 py-1.5 text-gray-950 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Klavier: Fly me to the Moon - Jonny May (Advanced)"
-              />
+          <form>
+            <div>
+              <label
+                htmlFor="deck-title"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Deck Title
+              </label>
+              <div className="relative mt-2 rounded-md shadow-sm">
+                <input
+                  type="text"
+                  name="deck-title"
+                  id="deck-title"
+                  className="block w-96 rounded-md border-0 py-1.5 text-gray-950 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Klavier: Fly me to the Moon - Jonny May (Advanced)"
+                />
+              </div>
             </div>
-          </div>
-          <div>
-            <label
-              htmlFor="number-of-bars"
-              className="block text-sm font-medium leading-6 text-white"
-            >
-              Number of bars
-            </label>
-            <div className="relative mt-2 rounded-md shadow-sm">
-              <input
-                type="number"
-                name="number-of-bars"
-                id="number-of-bars"
-                className="block w-16 rounded-md border-0 py-1.5 text-gray-950 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="0"
-              />
+            <div>
+              <label
+                htmlFor="number-of-bars"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Number of bars
+              </label>
+              <div className="relative mt-2 rounded-md shadow-sm">
+                <input
+                  type="number"
+                  name="number-of-bars"
+                  id="number-of-bars"
+                  className="block w-16 rounded-md border-0 py-1.5 text-gray-950 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="0"
+                />
+              </div>
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={notify}
+              >
+                Generate Deck
+              </button>
+              <Toaster />
             </div>
-          </div>
+          </form>
         </div>
       </main>
     </>
